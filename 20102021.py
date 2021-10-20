fc6c55f1f5bb67c9cb760d12b9c733641abed06d
John Peterson Valcourt <84840840+Peter2021-dot@users.noreply.github.com>
2021-10-21 02:11:03 +0800

    Update 20102021.py

--- 20102021.py
+++ 20102021.py
@@ -3,21 +3,18 @@ this program takes an integer less than 1000 and output the least power of 2 tha
 '''
 
 
-
+#create a list and add all power of 2 from 2 to 1000 in it
 list = []
-
 for i in range(1, 1001):
     list.append(pow(2, i))
 
 
-# for i in range(10):
-#     print(list[i])
-
+#input the number of cases from keyboard
 testcases = int(input("how many time do you want to play? "))
-count = 0
+count = 0  #initialize the couter of number of time user plays to 0
 while( count < testcases ):
-    numb = int(input())
-    for i in range(1000):
+    numb = int(input()) #input number from keyboard
+    for i in range(1000): #search in the list the least power of 2 grader than the number input by keyboard
         if numb < list[i]:
             print(list[i])
             break
