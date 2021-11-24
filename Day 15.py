cb4baee4031711b4034af34fe4ab9cb196d789eb
John Peterson Valcourt <84840840+Peter2021-dot@users.noreply.github.com>
2021-11-25 00:38:06 +0800

    Add files via upload
    Quick introduction to Python Lambda

---
+++ Day 15.py
@@ -0,0 +1,13 @@
+
+
+# add 15 to agument a and return the result
+x = lambda a : a + 15
+print(x(10)) # 25
+
+# Multiply argument a with argument b and return the result
+x = lambda a, b : a * b
+print(x(3, 6)) # 18
+
+# Summarize arguments a, b , c and return the result
+x = lambda a, b , c : a + c * b
+print(x(4, 3, 9)) # 31
